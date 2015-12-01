40d30f48897e15f680e2f8edd8c34f5ef7a24d8f
Ogleiv <przemyslaw.wielgo@gmail.com>
2015-12-01 23:20:43 +0100

    first commit

---
+++ collocations_common_tri.py
@@ -0,0 +1,46 @@
+
+def sort_collocations_tri(collocations):
+    collocations_list = []
+    for first_word in collocations.keys():
+        for second_word in collocations[first_word].keys():
+            for third_word in collocations[first_word][second_word].keys():
+                collocations_list.append((first_word, second_word, third_word, collocations[first_word][second_word][third_word]))
+    collocations_list = sorted(collocations_list, key=lambda collocation: collocation[3], reverse=True)
+    return collocations_list
+    
+def print_collocations_tri(collocations, number):
+    for c in collocations[:number]:
+        print c[0], c[1], c[2], c[3]
+
+def count_collocations_tri(collocations, first_word, second_word, third_word):
+    if first_word and second_word and third_word and first_word not in ['-'] and second_word not in ['-'] and third_word not in ['-']:
+        if first_word not in collocations.keys():
+            collocations[first_word] = dict()
+
+        if second_word not in collocations[first_word].keys():
+            collocations[first_word][second_word] = dict()
+       
+        if third_word not in collocations[first_word][second_word].keys():
+            collocations[first_word][second_word][third_word] = 1
+
+        else:
+            collocations[first_word][second_word][third_word] +=1
+
+def trim_word(word):
+    if not word:
+        return None
+
+    flag = False
+
+    if word[-1:] in ['.', ',', '\'', '\"', '-']:
+        word = word[:-1]
+        flag = True
+
+    if word and word[0] in ['.', ',', '\'', '\"', '-']:
+        word = word[1:]
+        flag = True
+
+    if flag:
+        word = trim_word(word)
+
+    return word
